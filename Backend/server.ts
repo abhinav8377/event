70a51a5c3b9f4a98878436a7c61318640a23165d
abhinav8377 <abhinav736t@gmail.com>
2026-07-22 10:14:37 +0530

    fix: events schedular changes

--- Backend/server.ts
+++ Backend/server.ts
@@ -3,6 +3,7 @@ import http from "http";
 import app from './app.js';
 import connectDB from './src/common/config/db.js';
 import { initSocketIO } from './src/socket.js';
+import { startLifecycleScheduler } from './src/modules/events/event.lifecycle.js';
 import dns from "dns";
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 const PORT = process.env.PORT || 5000;
@@ -11,6 +12,7 @@ connectDB()
   .then(() => {
     const server = http.createServer(app);
     initSocketIO(server);
+    startLifecycleScheduler();
     server.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}/`));
   })
   .catch((err: Error) => {
